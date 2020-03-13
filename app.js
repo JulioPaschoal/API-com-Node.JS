107783e96debbc9d4b104197eac1abc35bc805ba
JulioPaschoal <juliocpaschoal@gmail.com>
2020-03-13 19:05:59 -0300

    Melhorando o Projeto e Tratando Erros

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use(morgan('dev'));
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+//QUANDO NAO ENCONTRAR NENHUMA ROTA 
+app.use((req, res, next) => {
+    const erro = new Error('Rota não encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
