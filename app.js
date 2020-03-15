fceed44150be736d90757f1a4e6c5414608cde23
JulioPaschoal <juliocpaschoal@gmail.com>
2020-03-15 16:58:28 -0300

    Criando o Banco de Dados (MySQL)

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false })); //APENAS DADOS SIMPLES
 app.use(bodyParser.json()); //JSON DE ENTRADA NO BODY
 
+app.use((req, res, next) => {
+    res.header('Acces-Control-Allow-Origin', '*');
+    res.header('Acces-Control-Allow-Header',
+    'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({})
+    }
+    next();
+});
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
