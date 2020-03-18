cb19aa48e1ef0b4cec43f9f9b60d490cc04d422b
JulioPaschoal <juliocpaschoal@gmail.com>
2020-03-18 18:38:37 -0300

    Conectando a API ao Banco MySQL

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,57 +1,92 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 //RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Retorna os produtos'
-    });
+        mysql.getConnection((error, conn) => {
+            if (error) { return res.status(500).send({ error: error }) }
+            conn.query(
+                'SELECT * FROM produtos;',
+                (error, resultado, fields) => {
+                    if (error) { return res.status(500).send({ error: error }) }
+                    return res.status(200).send({ response: resultado})
+                }
+            )
+        });
 });
 
 //INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
-        const produto = {
-            nome: req.body.nome,
-            preco: req.body.preco
-            
-        };
-    res.status(201).send({
-        mensagem: 'Produto criado ',
-        produtoCriado: produto
-    });
+        mysql.getConnection((error, conn) => {
+            if (error) { return res.status(500).send({ error: error })}
+            conn.query(
+                'INSERT INTO produtos (nome, preco) VALUES (?,?)',
+                [req.body.nome, req.body.preco],
+                (error, resultado, fields) => {
+                    conn.release();
+                    if (error) { return res.status(500).send({ error: error }) }
+                    res.status(201).send({
+                        mensagem: 'Produto inserido com sucesso',
+                        id_produto: resultado.insertId
+                    });
+                }
+            )
+        });
 });
 
 //RETORNA PRODUTO ESPECIFICO POR ID
 router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
-
-    if (id === 'especial') {
-        res.status(200).send({
-            mensagem: 'Detalhe do produto',
-            id: id
-        });
-    }  else {
-        res.status(200).send({
-            mensagem: 'Voce passou um ID'
-        });
-    }
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produto = ?;',
+            [req.params.id_produto],
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({ response: resultado})
+            }
+        )
+    });
 });
 
 
 //ALTERAR UM PRODUTO 
 router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Produto alterado'
-    });
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error })}
+        conn.query(
+            'UPDATE produtos SET nome = ?, preco = ? WHERE id_produto = ?',
+            [req.body.nome, req.body.preco, req.body.id_produto],
+            (error, resultado, fields) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error }) }
+                res.status(202).send({
+                    mensagem: 'Produto Alterado com sucesso',
+                });
+            }
+        )
+    });;
 });
 
 
 
 //DELETAR UM PRODUTO
 router.delete('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Produto excluido'
-    });
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error })}
+        conn.query(
+            'DELETE FROM produtos WHERE id_produto = ?',
+            [ req.body.id_produto],
+            (error, resultado, fields) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error }) }
+                res.status(202).send({
+                    mensagem: 'Produto removido com sucesso',
+                });
+            }
+        )
+    });;
 });
 
 module.exports = router;
