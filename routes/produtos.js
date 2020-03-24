ba1a2c334b18e991ffd97739c63b8994f3bf7f69
JulioPaschoal <juliocpaschoal@gmail.com>
2020-03-24 12:50:53 -0300

    Aplicando as alterações nos Pedidos

--- routes/produtos.js
+++ routes/produtos.js
@@ -19,7 +19,7 @@ router.get('/', (req, res, next) => {
                                 preco: prod.preco,
                                 request: {
                                     tipo: 'GET',
-                                    descricao: 'Retorna todos os produtos',
+                                    descricao: 'Retorna os detalhe de um produtos especifico',
                                     url: 'http://localhost:3000/produtos/' + prod.id_produto
                                 }
                             }
@@ -41,10 +41,20 @@ router.post('/', (req, res, next) => {
                 (error, result, fields) => {
                     conn.release();
                     if (error) { return res.status(500).send({ error: error }) }
-                    res.status(201).send({
+                    const response = {
                         mensagem: 'Produto inserido com sucesso',
-                        id_produto: result.insertId
-                    });
+                        produtoCriado: {
+                            id_produto: result.id_produto,
+                            nome: req.body.nome,
+                            preco: req.body.preco,
+                            request: {
+                                tipo: 'Get',
+                                descricao: 'Retorna todos os produtos',
+                                url: 'http://localhost:3000/produtos'
+                            }
+                        }
+                    }
+                    return res.status(201).send(response);
                 }
             )
         });
@@ -59,8 +69,26 @@ router.get('/:id_produto', (req, res, next) => {
             [req.params.id_produto],
             (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({ response: result})
-            }
+
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        mensagem: 'Não foi encontrado produto com este ID'
+                    })
+                }
+                const response = {
+                        produto: {
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: result[0].preco,
+                        request: {
+                            tipo: 'Get',
+                            descricao: 'Retorna todos os produtos',
+                            url: 'http://localhost:3000/produtos'
+                        }
+                    }
+                }
+                return res.status(200).send(response);
+            }            
         )
     });
 });
@@ -76,9 +104,20 @@ router.patch('/', (req, res, next) => {
             (error, result, fields) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error }) }
-                res.status(202).send({
-                    mensagem: 'Produto Alterado com sucesso',
-                });
+                const response = {
+                    mensagem: 'Produto atualizado com sucesso',
+                    produtoAtualizado: {
+                        id_produto: req.body.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna os detalhe de um produtos especifico',
+                            url: 'http://localhost:3000/produtos/' + req.body.id_produto
+                        }
+                    }
+                }
+                return res.status(202).send(response);
             }
         )
     });;
@@ -96,9 +135,20 @@ router.delete('/', (req, res, next) => {
             (error, result, fields) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error }) }
-                res.status(202).send({
+                const response = {
                     mensagem: 'Produto removido com sucesso',
-                });
+                    request: {
+                        tipo:'POST',
+                        descricao: 'Insere um produto',
+                        url: 'http://localhost:3000/produtos',
+                        body: {
+                            nome: 'String',
+                            preco: 'Number'
+                        }
+
+                    }
+                }
+                return res.status(202).send(response);
             }
         )
     });;
